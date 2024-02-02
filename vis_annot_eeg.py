3d047e0593b6fe19057bdffb46a3e9dbf007fbd0
skywalkerylh <r11528050@g.ntu.edu.tw>
2024-02-02 14:02:36 +0800

    add drawrect,export annotation

--- vis_annot_eeg.py
+++ vis_annot_eeg.py
@@ -8,42 +8,96 @@ from plotly.subplots import make_subplots
 import dash
 from dash import dcc, html, Input, Output, State
 from dash.exceptions import PreventUpdate
-import plotly.express as px
 
-def extractTimeValuefromtxt(txt):
-    '''
-    return
-    values: int array
-    time : string list
-    '''
-    data = txt.readlines()
-    time= [line.split('\t')[0]for line in data]
-    values= [line.split('\t')[1].split('\n')[0] for line in data]
-    values= [int(line) for line in values]
-    values= np.array(values)
-    return values, time
+class read_raw_eeg:
+    def __init__(self):
+       pass
+    def extractTimeValuefromtxt(self,txt):
+        '''
+        return
+        values: int array
+        time : string list
+        '''
+        data = txt.readlines()
+        time= [line.split('\t')[0]for line in data]
+        values= [line.split('\t')[1].split('\n')[0] for line in data]
+        values= [int(line) for line in values]
+        values= np.array(values)
+        return values, time
 
-def valueConversion(data):
-    data = (data - 2048) * 100 /2048
-    return data
+    def valueConversion(self,data):
+        data = (data - 2048) * 100 /2048
+        return data
 
-def readEEGtxt(file_path):
-    txt= open(file_path, 'r')
-    data, time= extractTimeValuefromtxt(txt)
-    data= valueConversion(data)
-    return data,time
+  
+    def load_txt(self,file_path):
+        txt= open(file_path, 'r')
+        data, time= self.extractTimeValuefromtxt(txt)
+        data= self.valueConversion(data)
+        return data,time
 
 #folder_path= 'c:/Users/annie/Desktop/OHCA_data/P10'
+reader= read_raw_eeg()
 folder_path= "J:/我的雲端硬碟/thesis/OHCA_data/P10"
-file_path= os.path.join(folder_path, 'EEG1_event1.txt')
-data_ch1, time_ch1= readEEGtxt(file_path)
-file_path= os.path.join(folder_path, 'EEG2_event1.txt')
-data_ch2, time_ch2= readEEGtxt(file_path)
+file_path= os.path.join(folder_path, 'EEG1_nonseizure1.txt')
+data_ch1, time_ch1= reader.load_txt(file_path)
+file_path= os.path.join(folder_path, 'EEG2_nonseizure1.txt')
+data_ch2, time_ch2= reader.load_txt(file_path)
 assert data_ch1.shape== data_ch2.shape
 
 patient=10
 sf=128
+x_values_row = np.arange(0,len(data_ch1),1)
+y_values_row_ch1 = data_ch1
+y_values_row_ch2 = data_ch2
+print(y_values_row_ch1.shape)
+annot_export= pd.DataFrame(columns=['patient', 'start_idx', 'start_time', 'end_idx','end_time','duration_ms'])
 
+# app func
+def create_annotation_dataframe(start_pos, end_pos):
+    start_time = time_ch1[start_pos]
+    end_time = time_ch1[end_pos]
+    duration_in_ms = int((end_pos - start_pos)/sf * 1000)
+    
+    annotation_data = {
+        'patient': [patient],
+        'start_idx': [start_pos],
+        'start_time': [start_time],
+        'end_idx': [end_pos],
+        'end_time': [end_time],
+        'duration_ms': [duration_in_ms]
+    }
+
+    return pd.DataFrame(annotation_data, index=[0])
+
+def display_annotation_content(start_pos, start_time, end_pos, end_time, duration_in_ms):
+    
+    return (
+        f'start pos/time: {start_pos:.1f}/{start_time}\n'
+        f'end pos/time: {end_pos:.1f}/{end_time}\n'
+        f'duration: {duration_in_ms} ms'
+    )
+def record_delete_annotation(annotation, annot_export):
+    # annot_export is empty then add annot directly
+    if annot_export.shape[0]==0 :
+
+        annot_export= annot_export.append(annotation,ignore_index=True)
+        print('draw annotation')
+        print(annot_export.shape)
+    else:
+        # check if annot in annot_export, if in the annot_export means it's erasing the annot
+        is_in_pd= annot_export.isin(annotation.to_dict(orient='list')).all(axis=1)
+        print(is_in_pd)
+        if is_in_pd.any():
+            matching_row_idx= annot_export.index[is_in_pd].item()
+            annot_export=annot_export[is_in_pd]
+            print(f"Erase row {matching_row_idx} annotation ")
+            print(annot_export.shape)
+        else:
+            annot_export= annot_export.append(annotation,ignore_index=True)
+            print('draw annotation')
+            print(annot_export.shape)
+    return annot_export
 # web
 app = dash.Dash(__name__)
 app.layout = html.Div([
@@ -59,62 +113,122 @@ app.layout = html.Div([
     dcc.Input(id='input-width', type='number', placeholder='Enter width', value=1257),
     html.P('pixelsperinch:'),
     dcc.Input(id='input-pixelsperinch', type='number', placeholder='Enter pixelsperinch', value=105),
-   
+    # html.P('4. Upload   :'),
+    # html.P('EEG ch1 txt \n '),
+    # dcc.Upload(
+    # id='upload-ch1',
+    # children=html.Button('Upload a file'),
+    # multiple=False
+    # ),
+    # html.P('EEG ch2 txt  \n '),
+    # dcc.Upload(
+    # id='upload-ch2',
+    # children=html.Button('Upload a file'),
+    # multiple=False
+    # ),
     html.Button('Enter', id='update-button'),
-    dcc.Graph(id='plot-container', figure={})
-    #html.Div(id='plot-container')
     
+    dcc.Graph(id='plot-container',
+              figure={},
+              config={'modeBarButtonsToAdd':['drawrect','eraseshape']}),
+    #html.Div(id='plot-container')
+    html.Button('Export annotations', id='export-annotations-button'),
+    dcc.Download(id='export-annot-csv'),
+    html.Pre(id='annotation-info', children='annotation info\n')
+
 ])
 
 # Define callback to update graph
 @app.callback(
     Output('plot-container', 'figure'),
     [Input('update-button', 'n_clicks')],
-    [dash.dependencies.State('input-width', 'value'),
-     dash.dependencies.State('input-pixelsperinch', 'value')]
+    [State('input-width', 'value'),
+     State('input-pixelsperinch', 'value')]
 )
-def update_graph(n_clicks, width, pixelsperinch):
+def update_graph(n_clicks,width, pixelsperinch):
     if n_clicks is None:
         raise PreventUpdate
 
-    x_values_row = np.arange(0,len(data_ch1),1)
-    y_values_row_ch1 = data_ch1
-    y_values_row_ch2 = data_ch2
-    print(y_values_row_ch1.shape)
+    fig= make_subplots(rows=2, cols=1,shared_xaxes=True,subplot_titles=[f'F3-P3', f'F4-P4'])
+    fig.add_trace(go.Scatter(x=x_values_row, y=y_values_row_ch1, mode='lines'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=x_values_row, y=y_values_row_ch2, mode='lines'), row=2, col=1)
 
-    fig_row = make_subplots(rows=2, cols=1,shared_xaxes=True,subplot_titles=[f'F3-P3', f'F4-P4'])
-    fig_row.add_trace(go.Scatter(x=x_values_row, y=y_values_row_ch1, mode='lines'), row=1, col=1)
-    fig_row.add_trace(go.Scatter(x=x_values_row, y=y_values_row_ch2, mode='lines'), row=2, col=1)
-
+    # adjust plot based on monitor
     x_start = 0
     timebase = 30  # mm/sec
     width_mm = width/ (pixelsperinch / 2.54)*10 # pixel to mm
     x_end = int((sf-8)* ((width_mm / timebase)))
     initial_x_range= [x_values_row[x_start], x_values_row[x_end - 1]]
-    initial_y_range= [min(data_ch1),max(data_ch1)]
+    initial_y_range= [-50,50]
     
     # set ticks as string time
     tick_interval = 200
     tick_positions = np.arange(0, len(x_values_row), tick_interval)
     tick_labels = time_ch1[::tick_interval]
     
-    fig_row.update_layout(
+    
+    fig.update_layout(
         title=f'Patient {patient}',
         title_x= 0.5,
         width= width+20,
         showlegend=False,
-        margin=dict(l=10, r=10, b=0, t=40)
-       
-    )
-    fig_row.update_xaxes(range=initial_x_range,tickvals=tick_positions, ticktext=tick_labels ,row=1, col=1)
-    fig_row.update_xaxes(range=initial_x_range,tickvals=tick_positions, ticktext=tick_labels, row=2, col=1)
-    fig_row.update_yaxes(range=initial_y_range, row=1, col=1, fixedrange=True)
-    fig_row.update_yaxes(range=initial_y_range, row=2, col=1, fixedrange=True)
-
-
+        margin=dict(l=10, r=10, b=0, t=40),
+        dragmode='drawrect', # define dragmode
+        newshape=dict(line_color='cyan')
     
-    return fig_row
+    )
+    fig.update_xaxes(range=initial_x_range,tickvals=tick_positions, ticktext=tick_labels ,row=1, col=1)
+    fig.update_xaxes(range=initial_x_range,tickvals=tick_positions, ticktext=tick_labels, row=2, col=1)
+    fig.update_yaxes(range=initial_y_range, row=1, col=1, fixedrange=True)
+    fig.update_yaxes(range=initial_y_range, row=2, col=1, fixedrange=True)
 
+   
+
+    return fig
+
+@app.callback(
+    dash.dependencies.Output('annotation-info', 'children'),
+    [dash.dependencies.Input('plot-container', 'relayoutData')],
+    [dash.dependencies.State('annotation-info', 'children')])
+
+def rect_annotation_added(fig_data, content):
+    global annot_export
+    if fig_data is None:
+        return dash.no_update
+    
+    if 'shapes' in fig_data :
+
+        
+        line = fig_data['shapes'][-1]
+        
+        if line['type'] == 'rect':
+            start_pos= int(line['x0'])
+            end_pos= int(line['x1'])
+        
+        annotation= create_annotation_dataframe(start_pos,end_pos)
+        
+        content= ''
+        content= display_annotation_content(
+            start_pos, annotation['start_time'].item(),
+            end_pos, annotation['end_time'].item(),
+            annotation['duration_ms'].item()
+        )
+        annot_export= record_delete_annotation(annotation, annot_export)
+                
+    return content
+
+
+@app.callback(
+    Output("export-annot-csv", "data"),
+    [Input("export-annotations-button", "n_clicks")],
+    prevent_initial_call=True,
+)
+def export_csv(n_clicks):
+    if n_clicks is None:
+        raise PreventUpdate
+    startime= time_ch1[0].replace(':', '')
+    endtime  = time_ch1[-1].replace(':', '')
+    return dcc.send_data_frame(annot_export.to_csv,f'P{patient}_{startime}_{endtime}_annotation.csv')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
